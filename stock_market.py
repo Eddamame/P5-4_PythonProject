cb0f1eecc55386b68842c9d5643688b3d0925ae5
ignatiuslyh <229698934+ignatiuslyh@users.noreply.github.com>
2025-09-08 11:55:10 +0800

    Convert 'date' into datetime, removed EDA code, added notes

--- stock_market.py
+++ stock_market.py
@@ -6,21 +6,17 @@ import numpy as np
 dataset_url = 'https://github.com/Eddamame/INF1002_Stock/blob/main/Dataset/StockAnalysisDataset.csv?raw=true'
 df = pd.read_csv(dataset_url)
 
-#Exploratory Data Analysis (EDA) - Understanding your Data
-
-#First Look at Dataset
-print(df.head(5))
-
-#How many entries per Stock? 'Name' = column with Stock Name
-print(df.Name.value_counts())
-
-#Check Data Types of Columns
-print(df.info())
-
 #Change all floats to 2 decimal places
 df = df.round({'open': 2, 'high': 2, 'low': 2, 'close': 2})
 
-#Check updated dataset
-print(df.head(5))
+#Convert 'date' column into datetime format (YYYY-MM-DD)
+df['date'] = pd.to_datetime(df['date'])
 
-#Ignatius Update
+# Notes for reading from pandas (pd)
+# print(df.info()) -- Checks datatype of each column
+# print(df.head(5)) -- Shows first 5 rows
+# print(df.Name.value_counts()) -- Tells you how many rows per 'Name'
+# REMOVE NOTE FOR FINAL SUBMISSION ^
+
+# For those who need to use graphs,
+# Consider using libraries such as 'matplotlib','seaborn', 'plotnine' -- u guys can choose
