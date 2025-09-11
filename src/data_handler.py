bb656abe1bf6edbe6c2cb0a2dd08180039b4286e
ignatiuslyh <229698934+ignatiuslyh@users.noreply.github.com>
2025-09-12 00:23:26 +0800

    Moved filters earlier and streamlined code

--- src/data_handler.py
+++ src/data_handler.py
@@ -16,26 +16,23 @@ def data_handler(
     # Remove Missing Values
     df.dropna(inplace=True)
 
-    # Ensure correct data-types, raise errors if encountered
-    df['Name'] = df['Name'].astype(str)
-    df['date'] = pd.to_datetime(df['date'], errors='raise')
-    df['open'] = pd.to_numeric(df['open'], errors='raise')
-    df['close'] = pd.to_numeric(df['close'], errors='raise')
-    df['high'] = pd.to_numeric(df['high'], errors='raise')
-    df['low'] = pd.to_numeric(df['low'], errors='raise')
-    df['volume'] = pd.to_numeric(df['volume'], errors='raise')
-
-    # Ensure 2 decimal points for $
-    df = df.round({'open': 2, 'high': 2, 'low': 2, 'close': 2})
-
     # Filter specific Names
     if filterName:
         df = df[df['Name'].isin(filterName)]
+
+    # Ensure correct data-types, raise errors if encountered
+    df['Name'] = df['Name'].astype(str)
+    df['date'] = pd.to_datetime(df['date'], errors='raise')
+    for col in 'open','close','high','low','volume':
+        df[col] = pd.to_numeric(df[col],errors='raise')
 
     # Filter by time (years)
     if filterTime:
         start, end = filterTime
         df = df[(df['date'].dt.year >= start) & (df['date'].dt.year <= end)]
+
+    # Ensure 2 decimal points for $
+    df = df.round({'open': 2, 'high': 2, 'low': 2, 'close': 2})
 
     # Sort by name and date, then reset index & drop previous dataframe
     df.sort_values(by=['Name', 'date'], inplace=True)
