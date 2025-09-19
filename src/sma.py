d36bb2fcb29ae6f408f31704aa51833211df4721
project <123712365+AD22D-1@users.noreply.github.com>
2025-09-19 21:42:18 +0800

    SMA updated 1.0

--- src/sma.py
+++ src/sma.py
@@ -15,22 +15,10 @@ def calculate_sma(stock_name, window_size):
     # Filter the DataFrame for the selected stock
     filtered_df = df[(df['Name'] == stock_name) & (df['year'] > 2015)].copy()
     filtered_df = filtered_df.set_index('date')
-    closed_price = filtered_df['close']
-    n = window_size
-    i = 0
-    # Initialize an empty list to store SMA
-    sma=[]
+    closed_price = filtered_df['close'].tolist()
+  
+    sma = [sum(closed_price[i:i+window_size]) / window_size 
+           for i in range(len(closed_price) - window_size + 1)]
 
-    while i < len(closed_price):
-        # Calculate the average of current window
-        window_average = round(np.sum(closed_price[
-        i:i+window_size]) / window_size, 2)
-    
-        # Store the average of current
-        # window in moving average list
-        sma.append(window_average)
-    
-        # Shift window to right by one position
-        i += 1
-    filtered_df['sma'] = sma
+    filtered_df['sma'] = [None]*(window_size-1) + sma
     return filtered_df
